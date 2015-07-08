ec0f688beb148fc633fadd306be6edcaa89198d2
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2015-07-08 05:32:59 +0300

    adding dht sensor

--- src/ant.ino
+++ src/ant.ino
@@ -2,25 +2,30 @@
 #include <RF22ReliableDatagram.h>
 #include <RF22.h>
 #include <SPI.h>
+#include "DHT.h"
+
+#define DHTPIN 9
+#define DHTTYPE DHT11
 
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 0
 
 RF22ReliableDatagram manager(CLIENT_ADDRESS);
+DHT dht(DHTPIN, DHTTYPE);
 
 struct Pin {
   const int PIN;
   int state;
-  byte counter[2];
+  byte counter[5];
   byte failed[2];
   byte fixed[2];
 };
 
-Pin working = {8, LOW, {0, 0}, {8, 0}, {8, 1}};
-Pin pulse =   {7, LOW, {7, 0}, {    }, {    }};
-Pin weft =    {6, LOW, {0, 0}, {6, 0}, {6, 1}};
-Pin warp =    {5, LOW, {0, 0}, {5, 0}, {5, 1}};
-Pin manualStop = {4, LOW, {0, 0}, {4, 0}, {4, 1}}; 
+Pin working =    {8, LOW, {0, 0, 0, 0, 0}, {8, 0}, {8, 1}};
+Pin pulse =      {7, LOW, {7, 0, 0, 0, 0}, {    }, {    }};
+Pin weft =       {6, LOW, {0, 0, 0, 0, 0}, {6, 0}, {6, 1}};
+Pin warp =       {5, LOW, {0, 0, 0, 0, 0}, {5, 0}, {5, 1}};
+Pin manualStop = {4, LOW, {0, 0, 0, 0, 0}, {4, 0}, {4, 1}};
 
 // every 30 sec, send pulse count to nest
 const int pulseCountInterval = 5000;
@@ -38,6 +43,7 @@ TimedAction healthCheckCalculator = TimedAction(healthCheckInterval, healthCheck
 TimedAction reporter = TimedAction(reportInterval, report);
 
 void setup() {
+  dht.begin();
   pinMode(working.PIN, INPUT);
   pinMode(pulse.PIN, INPUT);
   pinMode(weft.PIN, INPUT);
@@ -94,7 +100,21 @@ void increasePulse() {
 void calculatePulse() {
   Serial.print("sending pulse count : ");
   Serial.println(pulse.counter[1]);
-  sendReliableMessage(pulse.counter);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  float hic = dht.computeHeatIndex(t, h, false);
+  pulse.counter[2] = h;
+  pulse.counter[3] = t;
+  pulse.counter[4] = hic;
+  sendReliableMessage(pulse.counter, true);
+  pulse.counter[2] = 0;
+  pulse.counter[3] = 0;
+  pulse.counter[4] = 0;
   pulse.counter[1] = 0;
 }
 
@@ -104,22 +124,35 @@ void checkError(struct Pin &pin, int high, int low) {
     Serial.print("error : ");
     Serial.println(pin.PIN);
 
-    sendReliableMessage(pin.failed);
+    sendReliableMessage(pin.failed, false);
   } else if (tempState == low && pin.state != tempState) {
     Serial.print("fixed : ");
     Serial.println(pin.PIN);
-    sendReliableMessage(pin.fixed);
+    sendReliableMessage(pin.fixed, false);
   }
   pin.state = tempState;
 }
 
-void sendReliableMessage ( byte data[]) {
+void sendReliableMessage ( byte data[], bool pulse) {
 
   Serial.print("sending data[0]:");
   Serial.println(data[0]);
   Serial.print("sending data[1]:");
   Serial.println(data[1]);
-  if (manager.sendtoWait(data, 2, SERVER_ADDRESS))  {
+
+  int length = 2;
+
+  if(pulse){
+    length = 5;
+    Serial.print("sending data[2]:");
+    Serial.println(data[2]);
+    Serial.print("sending data[3]:");
+    Serial.println(data[3]);
+    Serial.print("sending data[4]:");
+    Serial.println(data[4]);
+  }
+
+  if (manager.sendtoWait(data, length, SERVER_ADDRESS))  {
   //  uint8_t len = sizeof(buf);
   //  uint8_t from;
   //  if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
@@ -137,6 +170,3 @@ void sendReliableMessage ( byte data[]) {
   else
     Serial.println("sendtoWait failed");
 }
-
-
-
